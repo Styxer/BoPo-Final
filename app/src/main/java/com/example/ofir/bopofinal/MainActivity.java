a7e37ffc0e109b919f137a2e35c42e4e0b34a56a
Styxer <gennin@gmail.com>
2016-11-21 23:08:04 +0200

    first real commit to master, TODO: fix AndroidManifest.xml

--- app/src/main/java/com/example/ofir/bopofinal/MainActivity.java
+++ app/src/main/java/com/example/ofir/bopofinal/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.ofir.bopofinal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String username = intent.getStringExtra("username");
+        int age = intent.getIntExtra("age", -1);
+
+        TextView tvWelcomeMsg = (TextView) findViewById(R.id.tvWelcomeMsg);
+        EditText etUsername = (EditText) findViewById(R.id.etUserName);
+        EditText etAge = (EditText) findViewById(R.id.etAge);
+
+        // Display user details
+        String message = name + " welcome to your user area";
+        tvWelcomeMsg.setText(message);
+        etUsername.setText(username);
+        etAge.setText(age + "");
     }
 }
