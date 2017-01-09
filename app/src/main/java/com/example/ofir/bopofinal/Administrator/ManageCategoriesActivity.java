2114b3bab69f8d89546c02309d7da57163bddc78
Alonka <al8389@gmail.com>
2017-01-09 14:45:27 +0200

    commit with settings finished, delete account with delete user from all tables, administrator finished with reject category option

--- app/src/main/java/com/example/ofir/bopofinal/Administrator/ManageCategoriesActivity.java
+++ app/src/main/java/com/example/ofir/bopofinal/Administrator/ManageCategoriesActivity.java
@@ -24,6 +24,7 @@ public class ManageCategoriesActivity extends AppCompatActivity {
 
     TextView category_name, category_status, user_id, request_id, NoRequests, choose;
     Button btnAcceptRequests;
+    Button btnRejectRequests;
     RecyclerView recyclerView;
     RecyclerView.Adapter adapter;
     RecyclerView.LayoutManager layoutManager;
@@ -41,6 +42,7 @@ public class ManageCategoriesActivity extends AppCompatActivity {
         request_id = (TextView) findViewById(R.id.tvRequestID);
         NoRequests = (TextView) findViewById(R.id.tvNoCategories);
         btnAcceptRequests = (Button) findViewById(R.id.btnAcceptRequests);
+        btnRejectRequests = (Button) findViewById(R.id.btnRejectRequests);
         choose = (TextView)findViewById(R.id.tvChoose);
 
         if (AdministratorMainScreenActivity.noPendingRequests == true) {
@@ -48,6 +50,7 @@ public class ManageCategoriesActivity extends AppCompatActivity {
             category_name.setVisibility(View.INVISIBLE);
             category_status.setVisibility(View.INVISIBLE);
             btnAcceptRequests.setVisibility(View.INVISIBLE);
+            btnRejectRequests.setVisibility(View.INVISIBLE);
             choose.setVisibility(View.INVISIBLE);
             NoRequests.setVisibility(View.VISIBLE);
         } else if (AdministratorMainScreenActivity.noPendingRequests == false) {
@@ -55,6 +58,7 @@ public class ManageCategoriesActivity extends AppCompatActivity {
             category_name.setVisibility(View.VISIBLE);
             category_status.setVisibility(View.VISIBLE);
             btnAcceptRequests.setVisibility(View.VISIBLE);
+            btnRejectRequests.setVisibility(View.VISIBLE);
             choose.setVisibility(View.VISIBLE);
             NoRequests.setVisibility(View.INVISIBLE);
 
@@ -135,5 +139,49 @@ public class ManageCategoriesActivity extends AppCompatActivity {
             }
 
         });
+
+
+        btnRejectRequests.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                ArrayList<CategoryDetails> catList = ((ManageCategoriesRecyclerAdapter) adapter)
+                        .arrayList;
+
+                Response.Listener<String> responseListener3 = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            boolean success = jsonResponse.getBoolean("success");
+                            if (success) {
+                                Toast.makeText(ManageCategoriesActivity.this, "Category was rejected",
+                                        Toast.LENGTH_SHORT).show();
+                                ManageCategoriesActivity.this.startActivity(new Intent(ManageCategoriesActivity.this, AdministratorMainScreenActivity.class));
+                            } else {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(ManageCategoriesActivity.this);
+                                builder.setMessage("delete from pending list failed")
+                                        .setNegativeButton("Retry", null)
+                                        .create()
+                                        .show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                for (int i = 0; i < catList.size(); i++) {
+                    CategoryDetails singleCategory = catList.get(i);
+                    if (singleCategory.isSelected() == true) {
+                        DeleteCategoriesRequest deleteCategoriesRequest = new DeleteCategoriesRequest(catList.get(i).getRequestId()/*,catList.get(i).getRequestId()*/, responseListener3);
+                        RequestQueue queue3 = Volley.newRequestQueue(ManageCategoriesActivity.this);
+                        queue3.add(deleteCategoriesRequest);
+                    }
+                }
+            }
+
+        });
+
+
     }
 }
