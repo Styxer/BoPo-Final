1e849ec2b962dc777a026ddc0dd1746d534e6c32
Alonka <al8389@gmail.com>
2017-01-30 16:38:11 +0200

    commit with fixed bug in uploading picture from emulator

--- app/src/main/java/com/example/ofir/bopofinal/Profile/ProfileActivity.java
+++ app/src/main/java/com/example/ofir/bopofinal/Profile/ProfileActivity.java
@@ -1,13 +1,16 @@
 package com.example.ofir.bopofinal.Profile;
 
+import android.Manifest;
 import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -150,6 +153,11 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
                 datePickerDialog.show();
                 break;
             case R.id.ivProfilePhoto:
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(ProfileActivity.this,
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            1);
+                }
                 Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
                 break;
